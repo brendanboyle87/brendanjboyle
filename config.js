9c4ce7a35606cfc4e16aa8169141b192ca29abad
Brendan Boyle <brendanboyle87@gmail.com>
2019-02-18 17:30:23 -0500

    initial commit, remove boilerplate add info

--- config.js
+++ config.js
@@ -1,38 +1,34 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-  copyright: '© All rights reserved.',
-  disqusShortname: '',
-  postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
-  menu: [
-    {
-      label: 'Articles',
-      path: '/'
-    },
-    {
-      label: 'About me',
-      path: '/pages/about'
-    },
-    {
-      label: 'Contact me',
-      path: '/pages/contacts'
+    url: 'https://www.brendanjboyle.com',
+    title: 'Blog by Brendan Boyle',
+    subtitle: 'Brendan Boyle\'s Personal Site',
+    copyright: '© All rights reserved.',
+    disqusShortname: '',
+    postsPerPage: 4,
+    googleAnalyticsId: 'UA-73379983-2',
+    menu: [{
+            label: 'Articles',
+            path: '/'
+        },
+        {
+            label: 'About me',
+            path: '/pages/about'
+        },
+        {
+            label: 'Contact me',
+            path: '/pages/contacts'
+        }
+    ],
+    author: {
+        name: 'Brendan Boyle',
+        photo: '/bboyle.jpg',
+        bio: 'Software Engineer at Fidelity Investments. Former attorney who transitioned into tech after completing the Launch Academy bootcamp. This is my personal blog.',
+        contacts: {
+            email: 'brendanboyle87@gmail.com',
+            twitter: 'brendanboyle87',
+            github: 'brendanboyle87'
+        }
     }
-  ],
-  author: {
-    name: 'John Doe',
-    photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-    contacts: {
-      email: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '#',
-      vkontakte: '#'
-    }
-  }
 };
--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,1 +1,2 @@
-export { default } from './Page';
+export { default }
+from './Page';
